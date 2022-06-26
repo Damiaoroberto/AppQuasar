60f82d78e435eef0f54f4083b6fd423e52497d14
Damiaoroberto <106033055+Damiaoroberto@users.noreply.github.com>
2022-06-26 10:35:26 +0200

    fix(UX):correção de um bug no help

--- src/pages/help.vue
+++ src/pages/help.vue
@@ -1,10 +1,7 @@
 <template>
   <q-page class="-pa-lg">
     <h5 class="q-mt-none"></h5>
-    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-Recusandae officia tempora expedita perspiciatis adipisci
-ipsam et quisquam possimus, quos facilis omnis veritatis
-exercitationem. Assumenda laudantium est eos quidem dicta?</p>
+    <P>Ainda em desenvolvimento...</p>
 
   </q-page>
 </template>
